db392185df2651cf4000743c624aa7ba4a5a5137
The Lustri VA <thelustriva@gmail.com>
2022-07-30 09:00:33 +1000

    Added some basic documentation.

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Golem_Discord
-Golem's Discord bot presence.
+# Golem Bot README
+
+## get_greeting()
+
+Returns a greeting from content/greetings.json for all channels not serviced by GPT-3
+
+Returns:
+    _str_: _Coinflip between a funny written response and a funny glitch response._
+
+## get_openai_API_greeting()
+
+Take the contents of messages starting with '>hello' in certain channels and pulls a response from GPT-3
+
+Args:
+    Message (_str_): _A string of the message contents to which the bot should respond_
+
+Returns:
+    _str_: _A string response to the message taken from the GPT-3 API_
+
+## load_dotenv()
+
+Parse a .env file and then load all the variables found as environment variables.
+
+- _dotenv_path_: absolute or relative path to .env file.
+- _stream_: Text stream (such as `io.StringIO`) with .env content, used if
+  `dotenv_path` is `None`.
+- _verbose_: whether to output a warning the .env file is missing. Defaults to
+  `False`.
+- _override_: whether to override the system environment variables with the variables
+  in `.env` file.  Defaults to `False`.
+- _encoding_: encoding to be used to read the file.
+
+If both `dotenv_path` and `stream`, `find_dotenv()` is used to find the .env file.
+
+## on_message()
+
+Define a bot event that waits for a message in a channel and then responds with a greeting from the openai chatbot API
+
+Args:
+    Message (_discord.Message_): _A message object sent by a user in the server._
+
+## on_ready()
+
+None
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+"""
+Bot name: Golem
+Bot description: A chatbot that is clever, creative, and unhelpful.
+Bot author: Marco Lustri
+Bot version: 1.0.0
+Bot license: MIT
+
+Description: This is the main file for the bot. It is responsible for handling the bot's commands and responses.
+"""
+
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -5,62 +15,105 @@ import os
 import random
 import json
 import openai
-"""
-Define a python class for a Discord bot using the discord.py 1.7.3 API. The class should be able connect to the Discord server with ID 851041307493138432 using a token stored in the DISCORD_TOKEN value in a .env file. The bot's name is Golem. The class must be able to load extensions and register cogs.
-"""
 
-bot_invite_url = "https://discord.com/api/oauth2/authorize?client_id=710056301195165768&scope=bot&permissions=8"
 
-intents = discord.Intents.default()
-intents.members = True
+bot_invite_url = "https://discord.com/api/oauth2/authorize?client_id=710056301195165768&scope=bot&permissions=8"  # set the bot's invite url
 
-env_path = '.env'
+intents = discord.Intents.default()  # Set the bot's intents
+intents.members = True  # Include the 'members' privedged intent
+
+env_path = ".env"  # Load the bot's token from the .env file
 load_dotenv(dotenv_path=env_path)
 
 description = """
 Golem is a bot that can be used to manage the server.
-"""
+"""  # Set the bot's description
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")  # Set the OpenAI API key
 
-bot = commands.Bot(command_prefix='>', description=description, intents=intents)
+bot = commands.Bot(
+    command_prefix=">", description=description, intents=intents
+)  # Create the bot
 
-# define a bot event that waits for a message in a channel and then responds with a greeting from the openai chatbot API
 
-async def get_openai_API_greeting(message):
-    response = openai.Completion.create(engine="text-davinci-002", prompt=f"The following is a response from an AI chatbot named Golem. The chatbot is clever, creative, and unhelpful. It is responding to the human saying: {str(message)}", max_tokens=256)
-    return response.choices[0].text
+async def get_openai_API_greeting(message: str) -> str:
+    """_Take the contents of messages starting with '>hello' in certain channels and pulls a response from GPT-3_
+
+    Args:
+        message (_type_): _A string of the message contents to which the bot should respond_
+
+    Returns:
+        _str_: _A string response to the message taken from the GPT-3 API_
+    """
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"The following is a response from an AI chatbot named Golem. The chatbot is clever, creative, and unhelpful. It is responding to the human saying: {str(message)}",
+        max_tokens=256,
+    )  # Create a response from the OpenAI API
+    return response.choices[0].text  # Return the first response from the API
 
 
 async def get_greeting():
-    with open("content/greetings.json", "r", encoding="utf-8") as f:
-        greetings_blob = json.load(f)
-        greet_switch = random.choice([True, False])
-        if greet_switch:
+    """_Returns a greeting from content/greetings.json for all channels not serviced by GPT-3_
+
+    Returns:
+        _str_: _Coinflip between a funny written response and a funny glitch response._
+    """
+    with open("content/greetings.json", "r", encoding="utf-8") as f:  # Open the file
+        greetings_blob = json.load(f)  # Load the file
+        greet_switch = random.choice([True, False])  # Coinflip
+        if greet_switch:  # If the coinflip is true
             # return a random greeting from greetings.human_greetings in greetins.json
-            return random.choice(greetings_blob['greetings']["human_greetings"])
-        else:
-            return random.choice(greetings_blob['greetings']["glitches"])
+            return random.choice(
+                greetings_blob["greetings"]["human_greetings"]
+            )  # Return a random greeting
+        else:  # If the coinflip is false
+            return random.choice(
+                greetings_blob["greetings"]["glitches"]
+            )  # Return a random glitch
+
 
 @bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
+async def on_ready():  # When the bot is ready
+    print(
+        f"{bot.user.name} has connected to Discord!"
+    )  # Print the bot's name and connection status
+
 
 @bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
+async def on_message(message: discord.Message):  # When a message is sent
+    """__define a bot event that waits for a message in a channel and then responds with a greeting from the openai chatbot API__
 
-    allowed_channels = [873009485512323102, 868690558716108831]
-    
-    if message.channel.id in allowed_channels:
-        if message.content.startswith(">hello"):
-            await message.channel.send("API response fired off.")
-            greeting = await get_openai_API_greeting(message.content)
-            await message.channel.send(greeting)
-    elif message.channel.id not in allowed_channels:
-        if message.content.startswith('>hello'):
-            await message.channel.send(f"Heya. Sorry, I'm still lurking. I don't talk. I'm a bot. {await get_greeting()}")
+    Args:
+        message (_discord.Message): _A message object sent by a user in the server._
+    """
+    if message.author == bot.user:  # If the message is from the bot
+        return  # Return nothing
 
-if __name__ == '__main__':
-    bot.run(os.getenv("DISCORD_TOKEN"))
+    allowed_channels = [
+        873009485512323102,
+        868690558716108831,
+    ]  # Set the channels that the bot will respond to with a GPT-3 response
+
+    if (
+        message.channel.id in allowed_channels
+    ):  # If the message is in one of the allowed channels
+        if message.content.startswith(">hello"):  # If the message starts with '>hello'
+            await message.channel.send(
+                "API response fired off."
+            )  # Send a message to the channel
+            greeting = await get_openai_API_greeting(
+                message.content
+            )  # Get a greeting from the OpenAI API
+            await message.channel.send(greeting)  # Send the API greeting to the channel
+    elif (
+        message.channel.id not in allowed_channels
+    ):  # If the message is not in one of the allowed channels
+        if message.content.startswith(">hello"):  # If the message starts with '>hello'
+            await message.channel.send(
+                f"Heya. Sorry, I'm still lurking. I don't talk. I'm a bot. {await get_greeting()}"
+            )  # Send a pre-written message to the channel
+
+
+if __name__ == "__main__":  # If the bot is being run directly
+    bot.run(os.getenv("DISCORD_TOKEN"))  # Run the bot
